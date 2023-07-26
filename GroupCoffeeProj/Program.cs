e07b73846aacb839f087eeb99438b957b5d000e7
Tim Montague <tim.john.montague@gmail.com>
2023-07-26 16:10:59 -0400

    file

--- GroupCoffeeProj/Program.cs
+++ GroupCoffeeProj/Program.cs
@@ -2,9 +2,36 @@
 
 //using file_io;
 
-string filePath = "../../../CoffeeShop.txt";
+using GroupCoffeeProj;
 
+string filePath = "../../../CoffeeShop.txt";
+//Name, Category, Description, Price,Cream, Sugar, Hot)
 if (File.Exists(filePath) == false)
 {
     StreamWriter coffeeWriter = new StreamWriter(filePath);
+    coffeeWriter.WriteLine("black Coffy| | | 5.00");
+    coffeeWriter.WriteLine("Donut | hole| chocolate");
+    coffeeWriter.Close();
 }
+
+StreamReader coffeeWriter = new StreamReader(filePath);
+List<Items> menu =new List<Items>();
+
+while(true)
+{
+    //Name, Category, Description, Price,Cream, Sugar, Hot)
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
