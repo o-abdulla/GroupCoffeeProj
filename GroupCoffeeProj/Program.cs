fc76e500fc0a3a4d857baa9a7371e10723dfd50d
Omar Abdulla <osabdulla@gmail.com>
2023-07-26 16:37:47 -0400

    parts length

--- GroupCoffeeProj/Program.cs
+++ GroupCoffeeProj/Program.cs
@@ -8,19 +8,37 @@ string filePath = "../../../CoffeeShop.txt";
 //Name, Category, Description, Price,Cream, Sugar, Hot)
 if (File.Exists(filePath) == false)
 {
-    StreamWriter coffeeWriter = new StreamWriter(filePath);
-    coffeeWriter.WriteLine("black Coffy| | | 5.00");
-    coffeeWriter.WriteLine("Donut | hole| chocolate");
-    coffeeWriter.Close();
+    StreamWriter tempWriter = new StreamWriter(filePath);
+    tempWriter.WriteLine("Black Coffee| | | 5.00");
+    tempWriter.WriteLine("Donut | hole| chocolate");
+    tempWriter.Close();
 }
 
-StreamReader coffeeWriter = new StreamReader(filePath);
+StreamReader reader = new StreamReader(filePath);
 List<Items> menu =new List<Items>();
 
 while(true)
 {
     //Name, Category, Description, Price,Cream, Sugar, Hot)
+    string line = reader.ReadLine();
+    if (line == null)
+    {
+        break;
+    }
+    else
+    {
+        //Name, Category, Description, Price,Cream, Sugar, Hot)
+        string[] parts = line.Split("|");
+        Items i = new Items(parts[0], parts[1], parts[2], decimal.Parse(parts[3]));
+        if (parts.Length > 4)
+        {
 
+        }
+        
+        
+        
+        //Beverages b = new Beverages((parts[0], parts[1], parts[2], decimal.Parse(parts[3]), parts[4], parts[5], parts[6]));
+    }
 
 
 
