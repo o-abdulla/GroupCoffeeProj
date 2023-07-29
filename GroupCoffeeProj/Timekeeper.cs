d8733dea35dd1bc5b0042b510fe582b1bb56c491
Tim Montague <tim.john.montague@gmail.com>
2023-07-29 16:57:53 -0400

    Maid method for getting current time

---
+++ GroupCoffeeProj/Timekeeper.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimTool
+{
+    internal class Timekeeper
+    {
+        // MM/DD/YYYY
+       public static List<string> GetCerentMonthDayYear()
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string date = dateTime.ToString();
+            List<string> monthYear = new List<string>();
+            monthYear = date.Split('/').ToList();
+            monthYear[2] = monthYear[2].Substring(0, 4);
+            return monthYear;
+        }
+        // DD
+        public static string GetCerentDay() 
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string date = dateTime.ToString();
+            List<string> monthYear = new List<string>();
+            monthYear = date.Split('/').ToList();
+            monthYear[2] = monthYear[2].Substring(0, 4);
+            return monthYear[1];
+        }
+        //MM
+        public static string GetCerentMonth()
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string date = dateTime.ToString();
+            List<string> monthYear = new List<string>();
+            monthYear = date.Split('/').ToList();
+            monthYear[2] = monthYear[2].Substring(0, 4);
+            return monthYear[0];
+        }
+        //YYYY
+        public static string GetCerentYear()
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string date = dateTime.ToString();
+            List<string> monthYear = new List<string>();
+            monthYear = date.Split('/').ToList();
+            monthYear[2] = monthYear[2].Substring(0, 4);
+            return monthYear[2];
+        }
+        //YY
+        public static string GetCerentYearAbb()
+        {
+            DateTime dateTime = DateTime.UtcNow.Date;
+            string date = dateTime.ToString();
+            List<string> monthYear = new List<string>();
+            monthYear = date.Split('/').ToList();
+            monthYear[2] = monthYear[2].Substring(2, 2);
+            return monthYear[2];
+        }
+    }
+}
